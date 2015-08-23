abf5eab7eb5cea47ba37500d11c9622a5f97de9c
matijahorvat <matijaxhorvat@gmail.com>
2015-08-23 19:26:37 +0200

    Final version - with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix(...) returns a storage list for input and output (inverted) 
+## matrices. cacheSolve(...) takes a list and calculates new result or reads 
+## previously calculated and cached result.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix(...) simulates an object, which initializes with empty matrix
+## or a given matrix. Previously stored matrix is matched with the argument 
+## matrix and resets the previously calculated matrix on mismatch, which 
+## enforces consistency between input and output matrices. Getter / setter
+## functions are returned as list 'x', used in cacheSolve(x, ...) call.
+
+makeCacheMatrix <- function(in_matrix = matrix()) {
   
-  m <- NULL
+  out_matrix <- NULL
   
   set <- function(y) {
-    x <<- y
-    m <<- NULL
+    if(!identical(in_matrix, y)){
+      in_matrix <<- y
+      out_matrix <<- NULL
+    }
   }
   
   get <- function() 
-    x
+    in_matrix
     
   setinverse <- function(inverse) 
-    m <<- inverse
+    out_matrix <<- inverse
   
   getinverse <- function() 
-    m
+    out_matrix
   
   list(
     set = set
@@ -30,8 +38,21 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(x, ...) inverts argument 'x' in form of a list. If the matrix was 
+## inverted in the previous attempt, it will be contained in 'm' and returned to
+## the parent frame. If not, 'x' will be inverted in the current attempt 
+## (assuming invertibility) with result written to 'x'.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = makeCacheMatrix(), ...) {
+  m <- x$getinverse()
+  
+  if(!is.null(m)) {
+    message("getting cached inverse")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
